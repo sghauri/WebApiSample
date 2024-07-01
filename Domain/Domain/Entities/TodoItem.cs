a0685091ffd1f0d1ec6da295966da0eababefa1b
Shoaib Ghauri <shoaibghauri@gmail.com>
2024-07-01 18:33:00 +0500

    feature/db-context, add project for Domain.

---
+++ Domain/Domain/Entities/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class TodoItem : BaseEntity
+    {
+        [Required]
+        [StringLength(20)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set;}
+        public DateTime? LastUpdatedDate { get; set;}
+        [Required]
+        [StringLength(20)]
+        public string Status { get; set; }
+    }
+}
